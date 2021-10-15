dbfdd1e139de0957e294246276e731b0f0fe6681
Laurens Lamberts <laurens.lamberts@weareyou.com>
2021-10-15 14:27:13 +0200

    iOS physical device debugging scripted

--- mac/dev-setup-ios.sh
+++ mac/dev-setup-ios.sh
@@ -1,3 +1,12 @@
 sudo gem install cocoapods
 
+# IDB connector for iOS (react-native Flipper)
+brew tap facebook/fb
+brew install idb-companion
+brew install python3
+sudo python -m ensurepip --upgrade
+sudo pip install --upgrade pip
+sudo pip3 install fb-idb
+brew upgrade node
+
 echo "Setup iOS completed!"
