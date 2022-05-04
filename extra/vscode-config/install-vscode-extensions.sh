b690383389de434a616765b1b495161cb1c215af
Laurens Lamberts <laurens.lamberts@weareyou.com>
2022-05-04 22:01:25 +0200

    Daisy DSP debugger

--- extra/vscode-config/install-vscode-extensions.sh
+++ extra/vscode-config/install-vscode-extensions.sh
@@ -4,3 +4,4 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension kamikillerto.vscode-colorize
 code --install-extension PKief.material-icon-theme
+code --install-extension marus25.cortex-debug // For Daisy DSP programming debugging
