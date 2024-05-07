4378d2eacb17722fef7f61e9dbc63ca0d3dbe4ac
Laurens Lamberts <laurens.lamberts@iodigital.com>
2024-05-07 19:21:05 +0200

    added vscode configs

--- configs/vscode-config/install-vscode-extensions.sh
+++ configs/vscode-config/install-vscode-extensions.sh
@@ -4,4 +4,8 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension kamikillerto.vscode-colorize
 code --install-extension PKief.material-icon-theme
+code --install-extension piousdeer.horizon-theme-vscode-default-token-colors
+code --install-extension GitHub.copilot
+code --install-extension GitHub.copilot-chat
+code --install-extension codeandstuff.package-json-upgrade
 code --install-extension marus25.cortex-debug // For Daisy DSP programming debugging
