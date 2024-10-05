4c9d201d569596cce4d46134c160128bd460d769
Laurens <laurenslamberts@hotmail.com>
2024-10-05 13:56:37 +0200

    add spectacle to login open items

--- mac/extra-apps-general.sh
+++ mac/extra-apps-general.sh
@@ -6,3 +6,6 @@ brew install --cask microsoft-teams
 # brew install --cask microsoft-office
 brew install --cask spectacle
 # brew install --cask whatsapp
+
+# Add spectacle to login startup items
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Spectacle.app", hidden:false}'
