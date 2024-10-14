1e3eb8af43606de228ab32b982abf273aee3657c
Laurens <laurenslamberts@hotmail.com>
2024-10-14 10:15:36 +0200

    arrange desktop icons to grid

--- mac/mac-config.sh
+++ mac/mac-config.sh
@@ -1,6 +1,7 @@
 sh configure-dock.sh
 
 plutil -replace StandardViewSettings.IconViewSettings.arrangeBy -string grid ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
 
